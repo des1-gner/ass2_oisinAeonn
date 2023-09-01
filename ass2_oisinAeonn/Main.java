cbe86a2a103d4caffd3e5728b6378d6a1c4f674d
Oisin Aeonn s3952320 <odish1k9@gmail.com>
2023-09-01 14:33:28 +1000

    updated login

--- ass2_oisinAeonn/Main.java
+++ ass2_oisinAeonn/Main.java
@@ -1,7 +1,6 @@
 package ass2_oisinAeonn;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,8 +11,6 @@ public class Main extends Application {
   // The main method initializes the socialMediaAnalyzerApp object, and runs the Application.
 
   public static void main (String[] args) {
-
-    launch(args);
     
     // create an instance of the object socialMediaAnalyzerApp class. 
 
@@ -26,18 +23,19 @@ public class Main extends Application {
   }
 
   @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Create the login view and set it as the scene
-            LoginView loginView = new LoginView();
-            Scene scene = new Scene(loginView.getPane(), 400, 300);
 
-            primaryStage.setTitle("Login App");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+  public void start(Stage primaryStage) {
+  
+    // Create the login view and set it as the scene
+  
+    LoginView loginView = new LoginView(); // Assuming you have a class named LoginView
+  
+    Scene scene = new Scene(loginView.getPane(), 400, 300);
+
+    primaryStage.setTitle("Login App");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
+  }
 
 }
