2f92b2fbe83a5c25202119faf434226eb4ec87f0
Oisin Aeonn s3952320 <odish1k9@gmail.com>
2023-10-09 22:24:50 +1100

    remade some JUnit Tests 7/12

--- ass2_oisinAeonn/JUnit/DAHTest.java
+++ ass2_oisinAeonn/JUnit/DAHTest.java
@@ -1,97 +1,107 @@
 package ass2_oisinAeonn.JUnit;
 
-// Imports
-
-import org.junit.Before;
-import org.junit.After;
-import org.junit.Test;
-
 import ass2_oisinAeonn.Database.DatabaseConnector;
 import ass2_oisinAeonn.Model.Post;
 import ass2_oisinAeonn.Model.User;
-
-import static org.junit.Assert.*;
+import org.junit.*;
 
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.*;
+
 public class DAHTest {
 
-    private DatabaseConnector dbConnector;
-
     @Before
-    public void setUp() {
-        dbConnector = DatabaseConnector.getInstance();
-    }
-
-    @Test
-    public void testUpdateUserType() {
-        String username = "testUser";
-        String newType = "VIP";
-
-        String originalType = dbConnector.getUserTypeByUsername(username);
-
-        dbConnector.updateUserType(username, newType);
-
-        String updatedType = dbConnector.getUserTypeByUsername(username);
-
-        assertNotEquals(originalType, updatedType);
-        assertEquals(newType, updatedType);
-    }
-
-    @Test
-    public void testGetAllUsers() {
-        List<User> users = dbConnector.getAllUsers();
-        
-        assertEquals(10, users.size());
-    }
-
-    @Test
-    public void testDeleteUserByUsername() {
-        String usernameToDelete = "testUser";
-
-
-        assertTrue(dbConnector.checkIfUserExists(usernameToDelete));
-
-        dbConnector.deleteUserByUsername(usernameToDelete);
-
-        assertFalse(dbConnector.checkIfUserExists(usernameToDelete));
-    }
-
-    @Test
-    public void testInsertPost() {
-        int initialCount = dbConnector.getAllPosts().size();
-
-        Post testPost = new Post(initialCount, "testAuthor", "testContent", 10, 5, "01/01/2023 10:00", "testImage");
-        dbConnector.insertPost(testPost);
-
-        int afterInsertCount = dbConnector.getAllPosts().size();
-
-        assertEquals(initialCount + 1, afterInsertCount);
-    }
-
-    @Test
-    public void testGetPostsSharesDistribution() {
-        Map<String, Integer> distribution = dbConnector.getPostsSharesDistribution();
-        
-        assertNotNull(distribution);
-        assertTrue(distribution.containsKey("0-99"));
-        assertTrue(distribution.containsKey("100-999"));
-        assertTrue(distribution.containsKey("1000+"));
-    }
-
-    @Test
-    public void testGetUsersDistribution() {
-        Map<String, Integer> distribution = dbConnector.getUsersDistribution();
-
-        assertNotNull(distribution);
-        assertTrue(distribution.containsKey("admin"));
-        assertTrue(distribution.containsKey("VIP"));
-
+    public void setup() {
+        // Setup code before each test
     }
 
     @After
     public void tearDown() {
-
+        // Cleanup code after each test
     }
+
+    @Test
+    public void testGetAllPosts() {
+        List<Post> posts = DatabaseConnector.getAllPosts();
+        assertNotNull(posts);
+    }
+
+    @Test
+    public void testGetAllUsers() {
+        List<User> users = DatabaseConnector.getAllUsers();
+        assertNotNull(users);
+    }
+
+    @Test
+    public void testInsertAndDeletePost() {
+        Post post = new Post(-1, "Test Content", "TestAuthor", 0, 0, "01/01/2022 10:10", "testImage.jpg");
+        DatabaseConnector.insertPost(post);
+
+        List<Post> authorPosts = DatabaseConnector.getPostsByUsername("TestAuthor");
+        assertTrue(!authorPosts.isEmpty());
+
+        for (Post p : authorPosts) {
+            DatabaseConnector.deletePostById(p.getPostId());
+        }
+    }
+
+    @Test
+    public void testRegisterAndDeleteUser() {
+        User user = new User("testUsername", "Test", "User", "testPass", "Regular");
+        DatabaseConnector.registerUser(user);
+
+        boolean exists = DatabaseConnector.checkIfUserExists("testUsername");
+        assertTrue(exists);
+
+        DatabaseConnector.deleteUserByUsername("testUsername");
+    }
+
+    @Test
+    public void testUpgradeUser() {
+        User user = new User("testVIP", "Test", "VIP", "testPass", "standard");
+        DatabaseConnector.registerUser(user);
+        DatabaseConnector.upgradeUserToVIP("testVIP");
+
+        DatabaseConnector.deleteUserByUsername("testVIP");
+    }
+
+    @Test
+    public void testGetPostSharesDistribution() {
+        Map<String, Integer> distribution = DatabaseConnector.getPostsSharesDistribution();
+        assertNotNull(distribution);
+    }
+
+    @Test
+    public void testGetPostLikesDistribution() {
+        Map<String, Integer> distribution = DatabaseConnector.getPostsLikesDistribution();
+        assertNotNull(distribution);
+    }
+
+    @Test
+    public void testUsersDistribution() {
+        Map<String, Integer> distribution = DatabaseConnector.getUsersDistribution();
+        assertNotNull(distribution);
+    }
+
+    @Test
+    public void testGetPostById() {
+        Post post = DatabaseConnector.getPostById(1);
+        assertNotNull(post);
+    }
+
+    @Test
+    public void testGetPostContent() {
+        String content = DatabaseConnector.getPostContent(1);
+        assertNotNull(content);
+    }
+
+    @Test
+    public void testGetUserByUsername() {
+        User user = DatabaseConnector.getUserByUsername("testUsername");
+        assertNotNull(user);
+    }
+
+    // Add more tests as needed based on the methods you have.
 }
